02c120c7b9318d0cdad36524c391ac1a4fc8265d
Chris Wilson <chris.wilson@time.com>
2017-12-20 14:33:20 -0500

    Added some factchecks

--- R/analyze_route.R
+++ R/analyze_route.R
@@ -8,8 +8,8 @@ route <- read.csv("routes/data/optimal.csv",
   colClasses = c(rep("character", 3), rep("numeric", 6))                     
 )
 
-miles_per_kilometer = 0.621371
-square_miles_per_square_meters = 0.000386102 / 1000
+miles_per_kilometer = 0.621371;
+square_miles_per_square_meters = 1 / 2589988.11;
 
 route$area <- route$area * square_miles_per_square_meters
 
@@ -30,7 +30,8 @@ for (i in 2:NROW(route)) {
 distances <- as.numeric(distances)
 
 route["distance"] <- distances
-# sp docs specifie that spDist spits out kms when lat/lng is used
+
+# sp docs specify that spDist spits out kms when lat/lng is used
 route["distance"] <- route["distance"] * miles_per_kilometer
 
 #FACT-CHECKING
@@ -39,11 +40,13 @@ print(prettyNum(sum(distances), big.mark=","))
 #142,651, which matches the output from the previous script
 
 print(prettyNum(sum(route$area), big.mark=","))
-#2,955,007,862 looks right. Wikipedia says 3,119,885, but we're not counting water
+#2,955,008 looks right. Wikipedia says 3,119,885, but we're not counting water
+
+route$children <- round(route$children)
 
 #print(paste("total distance", prettyNum(sum(route$distance), big.mark=",")))
 print(paste("total children", prettyNum(sum(route$children), big.mark=",")))
 # The Census reports 40,138,328, but they're counting AK, HI and PR
 
 # We're ready to port over to the JavaScript viz!
-write.csv(route[,c("fips", "name", "long", "lat", "tz", "area", "children", "distance")], "../data/optimal_route.csv")
+write.csv(route[,c("fips", "name", "long", "lat", "tz", "area", "children", "distance")], "../data/optimal_route.csv", row.names = F)
